1c87ab5a9f18af746a293ad0df48d18a516ebab9
apurva@1197 <mpapurva@gmail.com>
2024-08-13 15:52:07 +0530

    --- Auto Git Commit ---

--- server/test_server.tf
+++ server/test_server.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 ##### Try to create one ubuntu server
-resource "aws_instance"  "this"  {
+resource "aws_instance" "this" {
   ami                     = "ami-0ad21ae1d0696ad58"
   instance_type           = "t2.micro"
   key_name                = "nishu"
