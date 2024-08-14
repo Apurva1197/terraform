9e2116783804f7070c9cd4a08c82380f33459a58
apurva@1197 <mpapurva@gmail.com>
2024-08-14 23:13:18 +0530

    --- Auto Git Commit ---

--- setup/main.tf
+++ setup/main.tf
@@ -26,7 +26,6 @@ resource "aws_dynamodb_table" "state_lock_table" {
 resource "aws_instance" "server_1" {
     ami = var.ami
     instance_type = var.instance_type
-    key_name = var.key_name
     tags = var.tags
 }
 
