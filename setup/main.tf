c9609dbb2ec21abe50db2ed993d91886f14f1388
apurva@1197 <mpapurva@gmail.com>
2024-08-15 00:19:34 +0530

    --- Auto Git Commit ---

--- setup/main.tf
+++ setup/main.tf
@@ -35,5 +35,21 @@ resource "aws_key_pair" "deployer" {
   public_key = 
 }
 
+resource "aws_security_group" "sg-webserver" {
+    vpc_id              = aws_vpc.vpc.id
+    name                = "webserver"
+    description         = "Security Group for Web Servers"
+
+    dynamic "ingress" {
+        for_each = local.inbound_ports
+        content {
+            from_port   = ingress.value
+            to_port     = ingress.value
+            protocol    = "tcp"
+            cidr_blocks = [ "0.0.0.0/0" ]
+        }
+    }
+
+
 
 
