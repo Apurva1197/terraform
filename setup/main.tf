41380ae598939810c6a837dabbdf2658f5f3b494
apurva@1197 <mpapurva@gmail.com>
2024-08-15 21:13:38 +0530

    --- Auto Git Commit ---

--- setup/main.tf
+++ setup/main.tf
@@ -63,8 +63,8 @@ resource "aws_instance" "server_1" {
     instance_type  = var.instance_type
     tags           = var.tags
     key_name       = var.key
-    # security_group = data.aws_security_group.terraform_sg.id
-    # user_data      = file("${path.module}/home/ubuntu/terrascript.sh")
+    security_group = data.aws_security_group.terraform_sg.id
+    user_data      = file("${path.module}/home/ubuntu/terrascript.sh")
 
 }
 
