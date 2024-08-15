f2c8ac65f1ce39b4b0a9fd2a3f3a013af773d6a9
apurva@1197 <mpapurva@gmail.com>
2024-08-15 22:08:18 +0530

    --- Auto Git Commit ---

--- setup/main.tf
+++ setup/main.tf
@@ -68,10 +68,10 @@ resource "aws_instance" "server_1" {
       #!/bin/bash
       sudo -i
       sudo apt update -y
-                     sudo apt install nginx -y
-                     echo "Welcome to my nginx homepage $HOSTNAME" > /var/www/index.nginx-debian.html
-                     sudo systemctl start nginx
-                     sudo systemctl enable nginx
+      sudo apt install nginx -y
+      echo "Welcome to my nginx homepage $HOSTNAME" > /var/www/index.nginx-debian.html
+      sudo systemctl start nginx
+                    sudo systemctl enable nginx
                      sudo systemctl status nginx
 
 
