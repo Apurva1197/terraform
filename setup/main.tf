9d8f0a8ee4bf4d895fb5ad23eeed0770b4f019c5
apurva@1197 <mpapurva@gmail.com>
2024-08-15 14:09:40 +0530

    --- Auto Git Commit ---

--- setup/main.tf
+++ setup/main.tf
@@ -35,9 +35,8 @@ resource "aws_security_group" "sg-webserver" {
     vpc_id              = aws_vpc.vpc.id
     name                = "webserver"
     description         = "Security Group for Web Servers"
-
     dynamic "ingress" {
-        for_each    = [80,22,8080, 3306]
+        for_each    = [80,22,8080,3306]
         protocol    =  "tcp"
         cidr_blocks = [ "0.0.0.0/0" ]
         
