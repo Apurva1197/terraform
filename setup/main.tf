5a47956694609d1295db27d5739b5cb454de6ac0
apurva@1197 <mpapurva@gmail.com>
2024-08-15 21:14:18 +0530

    --- Auto Git Commit ---

--- setup/main.tf
+++ setup/main.tf
@@ -62,7 +62,7 @@ resource "aws_instance" "server_1" {
     ami            = var.ami
     instance_type  = var.instance_type
     tags           = var.tags
-    key_name       = data.aws_key_pair.deployer
+    key_name       = data.aws_key_pair.deployer.id
     security_group = data.aws_security_group.terraform_sg.id
     user_data      = file("${path.module}/home/ubuntu/terrascript.sh")
 
