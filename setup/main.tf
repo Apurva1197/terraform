0fc98e436484bcebd3b7d573430e03cb1a15e0c9
apurva@1197 <mpapurva@gmail.com>
2024-08-15 17:39:22 +0530

    --- Auto Git Commit ---

--- setup/main.tf
+++ setup/main.tf
@@ -27,7 +27,8 @@ resource "aws_dynamodb_table" "state_lock_table" {
 # resource block for key creation
 resource "aws_key_pair" "deployer" {
   key_name   = deployer_key
-  public_key = file("${path.module}/home/ubuntu/.ssh/au")
+  public_key = file("${path.module}/home/ubuntu/.ssh/auauthorized_keys
+")
 }
 
 #resource block for security group creation
