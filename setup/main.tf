9cfb7928c9ccc614ceac78c56366d7f2636a6f13
apurva@1197 <mpapurva@gmail.com>
2024-08-15 16:37:09 +0530

    --- Auto Git Commit ---

--- setup/main.tf
+++ setup/main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "terraform_sg" {
   }
 
 # data block for sg
-data "aws_ami" "example" {
+data "aws_security" "example" {
   id = var.aws_ami_id
 }
 # resource block for server creation
