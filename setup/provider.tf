4bd82395f124d308e731e85391830a926fa61a96
apurva@1197 <mpapurva@gmail.com>
2024-08-15 20:00:41 +0530

    --- Auto Git Commit ---

--- setup/provider.tf
+++ setup/provider.tf
@@ -1,15 +1,15 @@
-# #Provider for authentication
-# provider "aws" {
-#     region = var.region
-# }
+#Provider for authentication
+provider "aws" {
+    region = var.region
+}
 
-# # # Terraform for S3 Backend
-# terraform {
-#     backend "s3" {
-#     bucket = "terraform-buckettt"
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#     dynamodb_table = "terraform_state_lock"
-#     }
-# }
+# # Terraform for S3 Backend
+terraform {
+    backend "s3" {
+    bucket = "terraform-buckettt"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform_state_lock"
+    }
+}
 
