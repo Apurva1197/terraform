c70fc9bb79268e95034029c17bea4827242e40f7
apurva@1197 <mpapurva@gmail.com>
2024-08-20 18:05:56 +0530

    --- Auto Git Commit ---

---
+++ all_vars/map.tf
@@ -0,0 +1,12 @@
+# variable "userage" {
+#   type = map
+#   default = {
+#     ankur  = 29
+#     adnan  = 22
+#     ashwin = 23
+#   }
+# }
+
+# output "userage" {
+#   value = "my name is ashwin and my age is ${lookup(var.userage, "ashwin")}"
+# }
