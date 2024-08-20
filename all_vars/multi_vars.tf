8f5d8647ae2a73064b6ebeadb50f1cf8c965e1fa
apurva@1197 <mpapurva@gmail.com>
2024-08-20 18:06:20 +0530

    --- Auto Git Commit ---

---
+++ all_vars/multi_vars.tf
@@ -0,0 +1,22 @@
+# variable "username" {}
+
+# output "printname" {
+#   value = "Hello ${var.username}"
+# }
+
+# variable "domain" {}
+
+# output "printdomain" {
+#     value = "My domain is ${var.domain}"
+# }
+
+# ################################################
+# variable "username" {}
+# variable "domain" {}
+
+# output "printname" {
+#   value = "Hello, ${var.username}, My domain is ${var.domain}"
+# }
+
+# # Command to run: terraform plan
+# # Command to pass variable from prompt:  terraform plan -var "username=chetan" -var "domain=sre"
