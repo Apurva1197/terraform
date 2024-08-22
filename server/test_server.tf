825efbfdfd9f944cc40903a9341c92875ad06792
apurva@1197 <mpapurva@gmail.com>
2024-08-22 17:01:53 +0530

    --- Auto Git Commit ---

--- server/test_server.tf
+++ server/test_server.tf
@@ -1,33 +1,33 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.61.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "5.61.0"
+#     }
+#   }
+# }
 
-provider "aws" {
-  # Configuration options
-  region = "ap-south-1"
-}
+# provider "aws" {
+#   # Configuration options
+#   region = "ap-south-1"
+# }
 
-##### Try to create one ubuntu server
-resource "aws_instance" "this" {
-  ami                     = "ami-0ad21ae1d0696ad58"
-  instance_type           = "t2.micro"
-  key_name                = "nishad"
-  tags = {
-    Enviorment = "staging"
-    owner = "jarvis"    
-  }
+# ##### Try to create one ubuntu server
+# resource "aws_instance" "this" {
+#   ami                     = "ami-0ad21ae1d0696ad58"
+#   instance_type           = "t2.micro"
+#   key_name                = "nishad"
+#   tags = {
+#     Enviorment = "staging"
+#     owner = "jarvis"    
+#   }
   
 
-}
+# }
 
-output "EC2" {
-  value = "here we are trying to create server"
-}
+# output "EC2" {
+#   value = "here we are trying to create server"
+# }
 
 
 
