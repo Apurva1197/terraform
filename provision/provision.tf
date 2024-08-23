3fd01d41f462cd282224bf127e19fa05c814af1f
apurva@1197 <mpapurva@gmail.com>
2024-08-23 15:34:33 +0530

    --- Auto Git Commit ---

--- provision/provision.tf
+++ provision/provision.tf
@@ -1,29 +1,39 @@
- 
-  resource "aws_instance" "this_aws_instance" {
-    //for_each = toset(var.imageid)
-    ami = "ami-0ad21ae1d0696ad58"
-    key_name = "nishad"
+provider "aws" {
+    region =  "us-west-2"
+    access_key = ""
+    secret_key = ""
+    //profile = "configs"
+}
+ resource "aws_instance" "this_aws_instance" {
+    ami = "ami-0eb5115914ccc4bc2"
+    vpc_security_group_ids = ["sg-081787a4571bd9c08"]
+    key_name = "anup_delete"
     instance_type = "t2.micro"
-   provisioner "file" {
-    source      = "/home/ubuntu/script.sh"
-    destination = "/media/"
+     
+     provisioner "file" {
+    source      = "readme.md"
+    destination = "/home/ec2-user/readme.md"
+      connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("${path.module}/id_rsa")
+    host     = "${self.public_ip}"
+  }
+  
+  
   }
    provisioner "local-exec" {
-    command = "echo ${self.private_ip} >> private_ips.txt"
+    command = "echo ${self.private_ip} >> /tmp/private_ips.txt "
   }
-    connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = "nishad"
-    host     = "self.public_ip"
+    provisioner "local-exec" {
+    working_dir = "/tmp/"
+    command = "echo ${self.private_ip} >> workingdir_private_ips.txt "
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "yum install httpd -y",
-      "systemctl start httpd",
-      "systemctl enable httpd"
-    ]
-  }
-    
-}  
+
+}
+  
+  
+  
+  
+ 
