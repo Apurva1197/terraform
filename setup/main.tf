908715d745a30109e93f8cb7710ceaa60c01a586
apurva@1197 <mpapurva@gmail.com>
2024-08-23 17:26:06 +0530

    --- Auto Git Commit ---

--- setup/main.tf
+++ setup/main.tf
@@ -1,6 +1,6 @@
 # S3 Bucket
 resource "aws_s3_bucket" "terraform-bucket" {
-  bucket = "terraform-"
+  bucket = "terraform-bucket-apurva"
 }
 
 resource "aws_s3_bucket_versioning" "terraform-state" {
